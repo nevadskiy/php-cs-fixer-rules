152eae82b15fda3240e3e5619a1080e887730930
nevadskiy <nevadskiy@gmail.com>
2021-06-19 17:22:22 +0300

    Fix directory

--- src/Style.php
+++ src/Style.php
@@ -14,12 +14,12 @@ class Style
 
     public function app(Finder $finder, array $rules = []): Config
     {
-        return $this->config($finder,  array_merge(require __DIR__.'./rules/app.php', $rules));
+        return $this->config($finder,  array_merge(require __DIR__.'./../rules/app.php', $rules));
     }
 
     public function lib(Finder $finder, array $rules = []): Config
     {
-        return $this->config($finder,  array_merge(require __DIR__.'./rules/lib.php', $rules));
+        return $this->config($finder,  array_merge(require __DIR__.'./../rules/lib.php', $rules));
     }
 
     protected function config(Finder $finder, array $rules)
