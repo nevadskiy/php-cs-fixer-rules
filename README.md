08b9163ed8bc995d61e328c5b33a6727f04fb91d
nevadskiy <nevadskiy@gmail.com>
2021-06-19 17:16:53 +0300

    Init package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PHP CS Fixer rules
+
+Made according to the guide: https://laravel-news.com/sharing-php-cs-fixer-rules-across-projects-and-teams
---
+++ src/Style.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Nevadskiy\PhpCsFixerRules;
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+class Style
+{
+    public static function new()
+    {
+        return (new static);
+    }
+
+    public function app(Finder $finder, array $rules = []): Config
+    {
+        return $this->config($finder,  array_merge(require __DIR__.'./rules/app.php', $rules));
+    }
+
+    public function lib(Finder $finder, array $rules = []): Config
+    {
+        return $this->config($finder,  array_merge(require __DIR__.'./rules/lib.php', $rules));
+    }
+
+    protected function config(Finder $finder, array $rules)
+    {
+        return (new Config())
+            ->setRules($rules)
+            ->setRiskyAllowed(true)
+            ->setFinder($finder);
+    }
+}
